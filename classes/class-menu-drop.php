d3109a3c33d91843e5a2f46257bd3eb4e3bc1c66
Mohammed Saimon <saimon2721@gmail.com>
2017-12-28 17:18:53 +0600

    menu rename, reposition, icon change works if all menus r set in the customizer area and dokan settings menu has no icon tag

--- classes/class-menu-drop.php
+++ classes/class-menu-drop.php
@@ -15,21 +15,21 @@
 class Menu_Dropdown_Custom_control extends WP_Customize_Control {
   protected $number_of_menus = 0;
   protected $all_menus = array();
+  public $html = array();
 
   public function set_all_menus() {
     $this->all_menus = get_option( 'dokan_customized_menus' );
     $this->number_of_menus = count( get_option( 'dokan_customized_menus' ) );
   }
-// substr( $this->link(), 43, -2 );
-  public function render_content() { 
-    $this->value = $this->value() == 'Settings <i class="fa fa-angle-right pull-right"></i>' ? 'Settings' : $this->value();
-    ?>
+
+  public function render_content() {
+      ?>
     <div class="menu-item-bar">
       <div class="menu-item-handle ui-sortable-handle">
-        <span class="item-type" aria-hidden="true"></span>
-          <span class="item-title" aria-hidden="true"><?php echo $this->value(); ?></span>
+        <span class="item-type" aria-hidden="true" style="float: left" >Menu Title</span>
+          <span class="item-title" aria-hidden="true">
             <span class="spinner"></span>
-            <span class="menu-item-title"><?php 'menu title' ?></span>
+            <span class="menu-item-title"></span>
           </span>
           <span class="item-controls">
             <button type="button" class="button-link item-edit" aria-expanded="false"><span class="screen-reader-text">Edit menu item: (no label) (Page)</span><span class="toggle-indicator" aria-hidden="true"></span></button>
@@ -37,33 +37,15 @@ class Menu_Dropdown_Custom_control extends WP_Customize_Control {
           </span>
       </div>
     </div>
-
-    <div class="menu-item-settings" id="menu-item-settings" style="height: 0; padding: 0; transition: all .5s ease">
+    <div class="menu-item-settings" id="menu-item-settings" style="height: 0; padding: 0; transition: all .5s ease">          
+    <?php foreach( $this->settings as $key => $value ) { ?>
       <p class="description description-thin">
         <label for="edit-menu-item-title">
           Title<br>
-          <input type="text" id="" placeholder="Menu Title" class="widefat edit-menu-item-title" name="<?php echo 'menu-item-title' ?>" value="<?php echo $this->value; ?>" <?php echo $this->link() ?>>
-        </label>
-      </p>      
-      <p class="description description-thin">
-        <label for="edit-menu-item-title">
-          Icon<br>
-          <input type="text" id="edit-menu-item-title" placeholder="Menu Title" class="widefat edit-menu-item-title" name="menu-item-title">
+          <input type="text" placeholder="Menu Title" class="widefat edit-menu-item-title" value="<?php echo $this->settings[$key]->value(); ?>" <?php echo $this->get_link( $key ) ?>>
         </label>
       </p>
-
-      <p class="description description-thin">
-        <label for="edit-menu-item-title">
-          Position<br>
-          <input type="text" id="edit-menu-item-title" placeholder="Menu Title" class="widefat edit-menu-item-title" name="menu-item-title" value="<?php $this->value() ?>">
-        </label>
-      </p>
-
-      <p class="field-link-target description description-thin">
-        <label for="edit-menu-item-target-118">
-          <input type="checkbox" id="edit-menu-item-target-118" class="edit-menu-item-target" value="_blank" name="menu-item-target">
-          Open link in a new tab        </label>
-      </p>
-    </div>
-  <?php }
+      <?php } ?>
+      </div>     
+ <?php }
 }
--- dokan-customizer.php
+++ dokan-customizer.php
@@ -18,12 +18,16 @@ class Dokan_Menu_Customizer {
 	private static $instance = null;
 	private $all_menus = array();
 	private $new_menus = array();
+	private $default_menus = array();
+	private $final_menus;
 
 	public function __construct() {
 		$this->init_hooks();
 		$this->define_constants();
 		$this->all_menus = get_option( 'dokan_customized_menus' );
-		$this->new_menus = get_theme_mod( 'menu_settings' );
+		$this->new_menus = $this->set_all_new_menus();
+		$this->set_default_menus();
+		$this->creating_final_menu_array();
 	}
 
 	public function define_constants() {
@@ -52,9 +56,26 @@ class Dokan_Menu_Customizer {
 				'capability'	=> 'edit_theme_options',
 				'transport'		=> 'refresh',
 			) );
+			$wp_customizer->add_setting( "menu_settings_icon[$key]", array(
+				'default' 		=> $menu['title'],
+				'type'			=> 'theme_mod',
+				'capability'	=> 'edit_theme_options',
+				'transport'		=> 'refresh',
+			) );
+			$wp_customizer->add_setting( "menu_settings_pos[$key]", array(
+				'default' 		=> $menu['title'],
+				'type'			=> 'theme_mod',
+				'capability'	=> 'edit_theme_options',
+				'transport'		=> 'refresh',
+			) );
 			$wp_customizer->add_control( new Menu_Dropdown_Custom_control( $wp_customizer, "menu_settings[$key]", array(
 				'label' 		=> __( 'Menu Control', 'dokan-lite' ),
-				'settings'		=> "menu_settings[$key]",
+				// 'settings'		=> "menu_settings[$key]",
+				'settings'		=> array(
+					"menu_settings[$key]",
+					"menu_settings_icon[$key]",
+					"menu_settings_pos[$key]",
+				),
 				'priority' 		=> $key,
 				'section'		=> 'menu_option',
 			) ) );
@@ -70,39 +91,143 @@ class Dokan_Menu_Customizer {
 		add_action( 'customize_controls_enqueue_scripts', array( $this, 'dokan_customizer_scripts' ) );
 		add_action( 'customize_register', array( $this, 'dokan_menu_cusotmize_register' ) );
 		add_action( 'customize_save_after', array( $this, 'dokan_save_customizer_settings' ) );
+		// add_action( 'init', array( $this, 'dokan_save_default_theme_mods' ) );
+		// add_filter( 'dokan_get_dashboard_nav', array( $this, 'dokan_set_dashboard_menus' ) );
 		// add_action( 'wp_footer', array( $this, 'dokan_customizer_preview' ), 12 );
 		// register backend specefic hooks
 		if ( is_admin() ) {
 			//
 		} else {
 			// register frontend specefic hooks	
-			add_filter( 'dokan_get_dashboard_nav', array( $this, 'dokan_get_dashboard_menus' ), 100 ); 
+			add_filter( 'dokan_get_dashboard_nav', array( $this, 'dokan_get_dashboard_menus' ), 100 );  
 			add_action('init', array($this, 'test'));
 		}
 	}
 	public function test() {
-		var_dump( $this->new_menus );
-		var_dump( $this->all_menus );
+		// var_dump( array_combine(['dashboard', 'products'], $this->new_menus) );
+		// var_dump( $this->default_menus );
+		var_dump( $this->final_menus );
+		// var_dump($this->new_menus );
+		
+		// var_dump( $this->all_menus );
 		// var_dump( get_option( 'dokan_customized_menus' ) );
 	}
 
-	public function dokan_save_customizer_settings() {
-		// update_option( 'dokan_customized_menus',  );
+	// public function dokan_sort_menu( $menus ) {
+	// 	return strtoupper($menus);
+	// }
+
+    public function dokan_save_default_theme_mods() {
+    	// set_theme_mod("menu_settings", array('dashboard' => 'dashboard new eita'));
+    	// set_theme_mod("menu_settings_icon", array('dashboard' => 'dashboard new icon'));
+    	// set_theme_mod("menu_settings_pos", array('dashboard' => '20'));
+    	// foreach ( $this->all_menus[0] as $key => $value ) {
+    	// 	var_dump($key);
+	    // 	set_theme_mod( "menu_settings", array( $key => $value['title'] ) );
+	    // 	// set_theme_mod( "menu_settings_icon", array( $key => $value['icon'] ) );
+	    // 	// set_theme_mod( "menu_settings_pos", array( $key => $value['pos'] ) );
+    	// }
+    	set_theme_mod("menu_settings", array( 'dashboard', 'dashboard new eita', 'products', 'products new eita' ) );
+    	// set_theme_mod("menu_settings_icon", array('dashboard' => 'dash icon'));
+    	// set_theme_mod("menu_settings_pos", array('dashboard' => '12'));
+
+    	// set_theme_mod("menu_settings", array('products' => 'product new eita'));
+    	// set_theme_mod("menu_settings_icon", array('products' => 'product icon'));
+    	// set_theme_mod("menu_settings_pos", array('products' => '13'));
+
+    	// set_theme_mod("menu_settings", array('orders' => 'orders new eita'));
+    	// set_theme_mod("menu_settings_icon", array('orders' => 'orders icon'));
+    	// set_theme_mod("menu_settings_pos", array('orders' => '14'));
+
+    	// set_theme_mod("menu_settings", array('withdraw' => 'withdraw new eita'));
+    	// set_theme_mod("menu_settings_icon", array('withdraw' => 'withdraw icon'));
+    	// set_theme_mod("menu_settings_pos", array('withdraw' => '15'));
+    	
+    	// set_theme_mod("menu_settings", array('settings' => 'settings new eita'));
+    	// set_theme_mod("menu_settings_icon", array('settings' => 'settings icon'));
+    	// set_theme_mod("menu_settings_pos", array('settings' => '15'));    	
+    	
+    	// set_theme_mod("menu_settings", array('coupons' => 'coupons new eita'));
+    	// set_theme_mod("menu_settings_icon", array('coupons' => 'coupons icon'));
+    	// set_theme_mod("menu_settings_pos", array('coupons' => '16'));    	
+
+    	// set_theme_mod("menu_settings", array('reviews' => 'reviews new eita'));
+    	// set_theme_mod("menu_settings_icon", array('reviews' => 'reviews icon'));
+    	// set_theme_mod("menu_settings_pos", array('reviews' => '17'));
+
+    	// set_theme_mod("menu_settings", array('reports' => 'reports new eita'));
+    	// set_theme_mod("menu_settings_icon", array('reports' => 'reports icon'));
+    	// set_theme_mod("menu_settings_pos", array('reports' => '18'));
+
+    	// set_theme_mod("menu_settings", array('booking' => 'booking new eita'));
+    	// set_theme_mod("menu_settings_icon", array('booking' => 'booking icon'));
+    	// set_theme_mod("menu_settings_pos", array('booking' => '18'));
+    }
+
+	public function set_default_menus() {
+		$menus = $this->all_menus;
+
+		foreach ( $menus[0] as $key => $value ) {
+			array_push($this->default_menus, $key );			
+		}
 	}
+
+	public function set_all_new_menus() {
+		$menus = get_theme_mod( 'menu_settings' );
+		$menus_icon = get_theme_mod( 'menu_settings_icon' );
+		$menus_pos = get_theme_mod( 'menu_settings_pos' );
+
+		if ( empty( $menus ) || empty( $menus_icon ) || empty( $menus_pos ) ) {
+			return;
+		}
+
+		$sorted_menus = array_map( null, $menus, $menus_icon, $menus_pos );
+
+		return $sorted_menus;
+	}
+
+	public function creating_final_menu_array() {
+		if ( empty( $this->new_menus ) ) {
+			return;
+		}
+
+		$final_menus = array();
+
+		foreach ( $this->default_menus as $key => $value ) {
+			$final_menus[$value] = $this->new_menus[$key];
+		}
+
+		$this->final_menus = $final_menus;
+	}
+
+	// public function dokan_save_customizer_settings() {
+		
+	// }
+
+	// public function dokan_set_dashboard_menus( $menus ) {
+	// 	$all_menus = array();
+
+	// 	foreach ( $menus as $key => $value ) {
+	// 		array_push( $this->default_menus, $key );
+	// 	}
+
+	// 	return $menus;
+	// }
+
 
 	public static function dokan_get_dashboard_menus( $urls ) {
 		$menus_to_save = array();
 
-		if ( ! empty( $this->new_menus ) ) {
+		if ( is_array( $this->final_menus ) && ! empty( $this->final_menus ) ) {
 			foreach ( $urls as $key => $value ) {
-				foreach ( $this->new_menus as $new_key => $new_value ) {
+				foreach ( $this->final_menus as $new_key => $new_value ) {
 					if ( strtolower( $value['title'] ) == $new_key ) {
 						$urls[$key] = array(
-							'title' => $new_value,
-							'icon' 	=> $value['icon'],
-							'url'  	=> $value['url'],
-							'pos' 	=> $value['pos'],
-						);						
+							'title' => $new_value[0],
+							'icon' 	=> $new_value[1],
+							'url' 	=> $value['url'],
+							'pos' 	=> $new_value[2],
+						);
 					}
 				}
 			}
